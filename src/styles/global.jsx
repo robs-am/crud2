063dd01d5095794e4f79e64dbb5886f6d2cc74b9
robs-am <roberta.amaro89@gmail.com>
2022-10-27 09:40:32 -0300

    add styles, font-family

---
+++ src/styles/global.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+* {
+    margin:0;
+    padding:0,
+    font-family: 'figtree', sans-serif;
+
+}
+
+
+`;
+
+export default Global;

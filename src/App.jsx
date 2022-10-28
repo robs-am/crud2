42b6395668144434e9b66eea557692fc82faffb8
robs-am <roberta.amaro89@gmail.com>
2022-10-28 20:26:29 -0300

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import UserList from "./components/users/userList"
+import UserList from "./components/users/userList";
 
-import "./App.css"
+import "./App.css";
 import Button from "./components/button/Button";
 
-
 function App() {
-  
-
   return (
     <div className="container">
-    <h1 className="title">CRUD React</h1>
-    <Button name={"Add User"} />
-     <UserList />
-     
+      <h1 className="title">CRUD React</h1>
+      <Button name={"Add User"} />
+      <UserList />
     </div>
-  )
+  );
 }
 
 export default App;
